55b7926b824fa780c75f8ca40b95403049ecf118
Maavcardoso <mariocardoso1997@gmail.com>
2022-02-23 16:45:36 -0300

    atualizando exercicio de 03-strings

--- conceitos_js/03-strings.js
+++ conceitos_js/03-strings.js
@@ -1,11 +1,24 @@
 // Algumas formas de manipular uma string em JS
-
 const text = "Teste"
 
 // retorna o tamanho da string
 const textSize = text.length
-console.log(`Quantidade de letras: ${textSize}`) 
+console.log(`\nQuantidade de letras: ${textSize}`) 
 
 // retorna um array quebrando a string por um delimitador
 const splittedText = text.split('s')
 console.log('\nArray com as posições separadas pelo delimitador s:', splittedText)
+
+// Busca um valor e o substitui.
+const replacedText = text.replace('Test', 'Leit')
+console.log('\nSubstituindo valor:', replacedText)
+
+// Retorna o conteúdo da posição da string
+const lastChar = text.slice(-1)
+console.log('\nUltima letra da string:', lastChar)
+
+const withoutLastChar = text.slice(0, -1)
+console.log('\nString sem a ultima letra:', withoutLastChar)
+
+const secondToEnd = text.slice(1)
+console.log('\nString a partir da segunda letra:', secondToEnd)
